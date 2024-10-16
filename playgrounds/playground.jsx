021a251ea1d3fff1873491fbe7ab9f2dafee5c20
rodrigo scola <rodrigo.sgarabotto.scola@gmail.com>
2024-10-15 21:23:53 -0300

    new languages

---
+++ playgrounds/playground.jsx
@@ -0,0 +1,131 @@
+function params(a, b) {}
+
+const i = (elem) => {
+	console.log(elem);
+};
+console.log('a');
+console.log('a');
+console.log('a');
+console.log('a');
+console.log(`a`);
+console.log('a');
+console.log('a');
+export function a() {
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	function b() {
+		const c = () => {
+			return () => {};
+		};
+	}
+	debugger;
+}
+const b = [
+	{
+		startPosition: {
+			row: 0,
+			column: 0,
+		},
+		//	endPosition: { row: 6, column: 1 },
+		startIndex: 0,
+		endIndex: 80,
+	},
+	[
+		{
+			startPosition: { row: 0, column: 0 },
+			//	endPosition: { row: 6, column: 1 },
+			startIndex: 0,
+			endIndex: 80,
+		},
+	],
+	// {
+	// 	startPosition: { row: 1, column: 1 },
+	// 	endPosition: { row: 5, column: 2 },
+	// 	startIndex: 16,
+	// 	endIndex: 78,
+	// },
+	// {
+	// 	startPosition: { row: 2, column: 2 },
+	// 	endPosition: { row: 4, column: 3 },
+	// 	startIndex: 33,
+	// 	endIndex: 75,
+	// },
+	// {
+	// 	startPosition: { row: 3, column: 10 },
+	// 	endPosition: { row: 3, column: 18 },
+	// 	startIndex: 61,
+	// 	endIndex: 69,
+	// },
+];
+
+function params(a, b) {}
+
+const i = (elem) => {
+	console.log(elem);
+};
+console.log('a');
+console.log('a');
+console.log('a');
+console.log('a');
+console.log(`a`);
+console.log('a');
+console.log('a');
+export function a() {
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	console.log();
+	function b() {
+		const c = () => {
+			return () => {};
+		};
+	}
+	debugger;
+}
+const b = [
+	{
+		startPosition: {
+			row: 0,
+			column: 0,
+		},
+		//	endPosition: { row: 6, column: 1 },
+		startIndex: 0,
+		endIndex: 80,
+	},
+	[
+		{
+			startPosition: { row: 0, column: 0 },
+			//	endPosition: { row: 6, column: 1 },
+			startIndex: 0,
+			endIndex: 80,
+		},
+	],
+	// {
+	// 	startPosition: { row: 1, column: 1 },
+	// 	endPosition: { row: 5, column: 2 },
+	// 	startIndex: 16,
+	// 	endIndex: 78,
+	// },
+	// {
+	// 	startPosition: { row: 2, column: 2 },
+	// 	endPosition: { row: 4, column: 3 },
+	// 	startIndex: 33,
+	// 	endIndex: 75,
+	// },
+	// {
+	// 	startPosition: { row: 3, column: 10 },
+	// 	endPosition: { row: 3, column: 18 },
+	// 	startIndex: 61,
+	// 	endIndex: 69,
+	// },
+];
+
+for (let i = 0; i < 100; i ++) {
+     console.log('htis')
+}
