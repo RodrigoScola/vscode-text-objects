024d2ec4d095d243a5551fd96ee7257ee49a5a4b
Rodrigo Scola <rodrigo.sgarabotto.scola@gmail.com>
2024-10-20 19:48:03 -0300

    start on change on keybinds

--- src/motions/selectors/json.ts
+++ src/motions/selectors/json.ts
@@ -2,16 +2,17 @@ import { Selector } from '../commands';
 import { JsQuery } from './javascript';
 
 export const JsonSelector: Selector = {
+	['outer.parameters']: '',
+	['inner.parameters']: '',
+	['outer.type']: '',
 	['inner.type']: '',
 	['outer.comment']: JsQuery['outer.comment'],
-	['outer.type']: '',
 	['outer.function']: '',
 	['inner.function']: '',
 	['inner.call']: '',
 	['outer.call']: '',
-	['outer.parameters']: '',
-	['inner.parameters']: '',
 	['outer.array']: JsQuery['outer.array'],
+
 	['outer.class']: '',
 	['inner.class']: '',
 	['outer.conditional']: '',
@@ -20,10 +21,10 @@ export const JsonSelector: Selector = {
 	['outer.loop']: '',
 	['inner.string']: JsQuery['inner.string'],
 	['outer.string']: JsQuery['outer.string'],
+
 	['outer.object']: JsQuery['outer.object'],
 	['outer.variable']: '',
 	['outer.rhs']: JsQuery['outer.rhs'],
 };
 
 //since the syntax is mostly the same, for now we can keep it the same
-
